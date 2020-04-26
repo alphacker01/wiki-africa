15c33a45fb0240142de6ee61f35a016e35872769
pabios <pabios.pabiosoft@gmail.com>
2020-04-26 23:08:28 +0200

    change package source

--- glossword_docs/misc/phpversion.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-print "\n".'PHP_VERSION_INT: '.PHP_VERSION;
-print "\n".'SERVER_SOFTWARE: '.getenv('SERVER_SOFTWARE');
-print "\n".'API            : '.PHP_SAPI;
-?>
